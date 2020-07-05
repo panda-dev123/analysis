e1ba40bab0c78739c0094a55b7f3c38c9c8f07c3
panda-dev <panda-dev123@outlook.com>
2020-07-06 04:15:00 +0800

    fix firebase

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -6,6 +6,13 @@ import "firebase/firestore";
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: "AIzaSyBj6nGrCAR5CIsaksTEDtmHjfoPOHMKUus",
+    authDomain: "driver-app-dev-53601.firebaseapp.com",
+    databaseURL: "https://driver-app-dev-53601.firebaseio.com/",
+    projectId: "driver-app-dev-53601",
+    storageBucket: "driver-app-dev-53601.appspot.com",
+    messagingSenderId: "625683953041",
+    appId: "1:625683953041:web:eaf8873cb33948cc065630",
+    measurementId: "G-ZG1PRS5Q03"
   });
 }
 
