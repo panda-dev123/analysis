d03c3080617b023a20fdb2a97455947cc96a694b
panda-dev <panda-dev123@outlook.com>
2020-07-06 04:19:48 +0800

    firebase

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -2,18 +2,22 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+
+const prodConfig = {
+  apiKey: "AIzaSyBj6nGrCAR5CIsaksTEDtmHjfoPOHMKUus",
+  authDomain: "driver-app-dev-53601.firebaseapp.com",
+  databaseURL: "https://driver-app-dev-53601.firebaseio.com/",
+  projectId: "driver-app-dev-53601",
+  storageBucket: "driver-app-dev-53601.appspot.com",
+  messagingSenderId: "625683953041",
+  appId: "1:625683953041:web:eaf8873cb33948cc065630",
+  measurementId: "G-ZG1PRS5Q03"
+}
 // Make sure it hasn't already been initialized
+
+const config = prodConfig
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyBj6nGrCAR5CIsaksTEDtmHjfoPOHMKUus",
-    authDomain: "driver-app-dev-53601.firebaseapp.com",
-    databaseURL: "https://driver-app-dev-53601.firebaseio.com/",
-    projectId: "driver-app-dev-53601",
-    storageBucket: "driver-app-dev-53601.appspot.com",
-    messagingSenderId: "625683953041",
-    appId: "1:625683953041:web:eaf8873cb33948cc065630",
-    measurementId: "G-ZG1PRS5Q03"
-  });
+  firebase.initializeApp(config)
 }
 
 export default firebase;
